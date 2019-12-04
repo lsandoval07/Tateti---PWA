8c38ac333cf26e59bb54ce9e8276ae943a690e4f
LeOlS <57971670+lsandoval07@users.noreply.github.com>
2019-12-03 21:44:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
 //Instalar el service worker
-self.addEventListener("install", evt=>{console.log("El service worker se instaló")});
+self.addEventListener("install", evt=>{console.log("El service worker se instaló");});
+//Activar el service worker
+self.addEventListener("activate", evt=> {HTMLFormControlsCollection.log("El service worker se activó.");});

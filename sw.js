2575d3d3183f3712f8583b4ecd63ae063b057072
LeOlS <57971670+lsandoval07@users.noreply.github.com>
2019-12-03 22:58:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
+const nombreCache="Sitio-Caché";
+const elementos=["/","/index.html","/css/estilo.css","/js/accion.js"]
 //Instalar el service worker
-self.addEventListener("install", evt=>{console.log("El service worker se instaló");});
+self.addEventListener("install", evt=>{
+    //console.log("El service worker se instaló");
+    evt.waitUntil(
+        caches.open(nombreCache).then((cache)=>
+        {console.log("Definimos el cache predeterminado.");
+        cache.addAll(elementos);
+    })
+    );
 //Activar el service worker
 self.addEventListener("activate", evt=> {console.log("El service worker se activó.");});
 //Eventos Fetch (fetch request o pedido de búsqueda).
-
 self.addEventListener("fetch", evt=>{console.log("Se atrapó el evento: ", evt);});

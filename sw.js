11f49d7590f5c9b189b8a728b268028814138528
LeOlS <57971670+lsandoval07@users.noreply.github.com>
2019-12-03 21:57:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,3 +2,6 @@
 self.addEventListener("install", evt=>{console.log("El service worker se instaló");});
 //Activar el service worker
 self.addEventListener("activate", evt=> {console.log("El service worker se activó.");});
+//Eventos Fetch (fetch request o pedido de búsqueda).
+
+self.addEventListener("fetch", evt=>{console.log("Se atrapó el evento: ", evt);});

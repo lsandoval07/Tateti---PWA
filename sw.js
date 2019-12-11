98bfd998f6c2361a70c0fcdd98b459d268760237
LeOlS <57971670+lsandoval07@users.noreply.github.com>
2019-12-10 21:25:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,40 @@
-const nombreCache="Sitio-Caché";
-const elementos=["/","/index.html","/css/estilo.css","/js/accion.js"]
-//Instalar el service worker
-self.addEventListener("install", evt=>{
-    //console.log("El service worker se instaló");
-    evt.waitUntil(
-        caches.open(nombreCache).then((cache)=>
-        {console.log("Definimos el cache predeterminado.");
-        cache.addAll(elementos);
-    })
-    );
+const nombreCache="sitio-cache-v2";// era sin v2
+
+const elementos=["https://cheloelian.github.io/TatetiPwa/","index.html","ejer6ok0711.css","ejer6ok0711.js","manifest.json","app.js"];
+
+// Instalar el service worker
+self.addEventListener("install", evt =>
+{
+	//console.log("El Service Worker se instalo");
+	evt.waitUntil(
+		caches.open(nombreCache).then((cache)=> 
+		{
+			console.log("Definimos el Cache Predeterminado.");
+			cache.addAll(elementos);
+		})
+		);
+});
 //Activar el service worker
-self.addEventListener("activate", evt=> {console.log("El service worker se activó.");});
-//Eventos Fetch (fetch request o pedido de búsqueda).
-self.addEventListener("fetch", evt=>{console.log("Se atrapó el evento: ", evt);});
+self.addEventListener("activate", evt =>{
+	evt.waitUntil(
+		caches.keys().then(Keys => {
+			console.log(keys);
+			return Promise.all(keys
+				.filter(Key => key !== nombreCache)
+				.map(key => caches.delete(key))
+				)
+		})
+		);
+});
+//Eventos Fetch - fetch request o pedido de busqueda
+self.addEventListener("fetch", evt =>{
+	console.log("Se atrapo el Evento: ", evt);
+	evt.respondWith(
+		caches.match(evt.request).then(cacheRes => {
+			return  cacheRes || fetch(evt.request)
+		})
+	);
+});
+
+
+
